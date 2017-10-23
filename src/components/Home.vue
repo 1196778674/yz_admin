436a19acef59693a7f634e3c99069d3042123ebf
liufei <1196778674@qq.com>
2017-10-23 16:32:49 +0800

    模板

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button @click="change">click</button>
+    
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import Login from '@/components/Login'
 
 Vue.use(Router)
@@ -14,8 +14,8 @@ export default new Router({
     },
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
