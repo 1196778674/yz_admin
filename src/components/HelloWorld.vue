20bbd42af326632728572e371bcf1ada1870b852
liufei <1196778674@qq.com>
2017-10-23 16:30:03 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="change">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  },
+  created: function(){
+    this.msg = '优翔客户管理系统'
+  },
+  methods: {
+    change () {
+      this.msg = '优翔客户管理系统!'
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="yx_login">
+		denglu 
+	</div>
+</template>
+<script>
+	export default{
+		name: 'Login',
+		data(){
+			return {
+				
+			}
+		},
+	}
+</script>
+<style scoped>
+	
+</style>
