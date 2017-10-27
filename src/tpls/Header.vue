31ded8a465676c7f1b994875966a6a1954922525
liufei <1196778674@qq.com>
2017-10-27 15:01:02 +0800

    css

--- src/tpls/Header.vue
+++ src/tpls/Header.vue
@@ -4,7 +4,7 @@
         <header>
             <!-- logo -->
             <div class="am-fl tpl-header-logo">
-                <a href="javascript:;"><img src="../assets/img/logo.png" alt=""></a>
+                <a href="javascript:;"><img src="../assets/img/logo_1.png" alt=""></a>
             </div>
             <!-- 右侧内容 -->
             <div class="tpl-header-fluid">
