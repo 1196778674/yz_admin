9b5481f21eeac967444722e0dbb384c374266208
liufei <1196778674@qq.com>
2017-10-31 14:18:30 +0800

    form

--- src/components/Forms/Form.vue
+++ src/components/Forms/Form.vue
@@ -3,8 +3,29 @@
 		<div v-if="type == 'project'">
 			<projectform></projectform>
 		</div>
-		<div v-else>
+		<div v-if="type == 'articles'">
 			<articlesform></articlesform>
+		</div>
+		<div v-if="type == 'category'">
+			<categoryform></categoryform>
+		</div>
+		<div v-if="type == 'center'">
+			<centerform></centerform>
+		</div>
+		<div v-if="type == 'consultation'">
+			<consultationform></consultationform>
+		</div>
+		<div v-if="type == 'equipment'">
+			<equipmentform></equipmentform>
+		</div>
+		<div v-if="type == 'modular'">
+			<modularform></modularform>
+		</div>
+		<div v-if="type == 'personnel'">
+			<personnelform></personnelform>
+		</div>
+		<div v-if="type == 'tags'">
+			<tagsform></tagsform>
 		</div>
 	</div>
 </template>
@@ -12,6 +33,14 @@
 <script>
 	import Projectform from './Projectform';
 	import Articlesform from './Articlesform';
+	import Categoryform from './Categoryform';
+	import Centerform from './Centerform';
+	import Consultationform from './Consultationform';
+	import Equipmentform from './Equipmentform';
+	import Modularform from './Modularform';
+	import Personnelform from './Personnelform';
+	import Tagsform from './Tagsform'
+
 	export default {
 		name: 'Form',
 		data () {
@@ -25,6 +54,13 @@
 		components: {
 			"projectform" : Projectform,
 			"articlesform" : Articlesform,
+			"categoryform" : Categoryform,
+			"centerform" : Centerform,
+			"consultationform" : Consultationform,
+			"equipmentform" : Equipmentform,
+			"modularform" : Modularform,
+			"personnelform" : Personnelform,
+			"tagsform" : Tagsform,
 		},
 	}
 </script>
