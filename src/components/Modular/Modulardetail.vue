26240f57363be363a0cb4d01d58a9906ff044224
liufei <1196778674@qq.com>
2017-11-06 13:42:20 +0800

    模块详情

--- src/components/Modular/Modulardetail.vue
+++ src/components/Modular/Modulardetail.vue
@@ -50,39 +50,59 @@
 		        </tr>
 		        <tr>
 		            <td class="title no-right">作用部位</td>
-		            <td colspan="5" class="no-right">腰，腿</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="part in detail.module_working_part_labels">
+		            		{{part.name}}
+		            	</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">作用功能</td>
-		            <td colspan="5" class="no-right">检测、报告</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="functs in detail.module_function_labels">
+		            		{{functs.name}}
+		            	</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">描述</td>
-		            <td colspan="5" class="no-right">进行精准运动诊疗的基础。量化、科学的评估可综合而深度的反映出受评者的体脂率、姿势表现、平衡能力、本体感觉、肌肉能量、关节灵活度等，在测试中发现已经存在或潜在的问题，从而有针对性的设计解决方案。</td>
+		            <td colspan="5" class="no-right">{{detail.description}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">禁忌症</td>
-		            <td colspan="5" class="no-right">孕妇、骨质疏松、心血管疾病、损伤达手术要求、静脉曲张(严重)、脊髓压迫(严重)、皮肤疾病、皮肤破损、骨折</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="contraindications in detail.module_contraindications_labels">
+		            		{{contraindications.name}}
+		            	</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">设备</td>
-		            <td colspan="5" class="no-right">空气置换身体成分测试系统、FMS功能性运动筛查套件、Y 平衡检测仪、便携式肌力测试与关节活动度计</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="equipment in detail.module_equipment">
+		            		{{equipment.name}}
+		            	</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">用品</td>
-		            <td colspan="5" class="no-right">BODPOD呼吸管、 一次性拖鞋</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="supplies in detail.module_supplies">
+		            		{{supplies.name}}
+		            	</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">注意事项</td>
-		            <td colspan="5" class="no-right">空气置换身体成分测试系统、FMS功能性运动筛查套件、Y 平衡检测仪、便携式肌力测试与关节活动度计</td>
+		            <td colspan="5" class="no-right">{{detail.considerations}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">不良反应</td>
-		            <td colspan="5" class="no-right">空气置换身体成分测试系统、FMS功能性运动筛查套件、Y 平衡检测仪、便携式肌力测试与关节活动度计</td>
+		            <td colspan="5" class="no-right">{{detail.adverse_reaction}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">备注</td>
-		            <td colspan="5" class="no-right">进行精准运动诊疗的基础。量化、科学的评估可综合而深度的反映出受评者的体脂率、姿势表现、平衡能力、本体感觉、肌肉能量、关节灵活度等，在测试中发现已经存在或潜在的问题，从而有针对性的设计解决方案。</td>
+		            <td colspan="5" class="no-right">{{detail.remark}}</td>
 		        </tr>
 		    </tbody>
 		</table>
@@ -90,7 +110,7 @@
 			
 			<ul>
 				<li><button type="button" class="am-btn am-btn-secondary" @click="prevPage()">上一页</button></li>
-				<li><button type="button" class="am-btn am-btn-secondary" @click="editPage()">编辑</button></li>
+				<li><button type="button" class="am-btn am-btn-secondary" @click="editPage(edit_id)">编辑</button></li>
 				<li><button type="button" class="am-btn am-btn-secondary" @click="deletePage($event)">删除</button></li>
 				<li><button type="button" class="am-btn am-btn-secondary" @click="printPage()">打印</button></li>
 				<li><button type="button" class="am-btn am-btn-secondary" @click="nextPage()">下一页</button></li>
@@ -105,39 +125,8 @@
 		name: 'Modulardetail',
 		data () {
 			return {
-				detail: {
-					"id": '',
-			        "code": "",
-			        "name": "",
-			        "center_id": '',
-			        "service_time": '',
-			        "service_after_time": '',
-			        "whether_medical": "",
-			        "min_age_limit": '',
-			        "max_age_limit": '',
-			        "gender_limit": '',
-			        "considerations": "",
-			        "adverse_reaction": "",
-			        "description": "",
-			        "expected_cost": "",
-			        "remark": "",
-			        "created_at": "",
-			        "updated_at": "",
-			        "deleted_at": '',
-			        "center_name": "",
-			        "job_grades": [],
-			        "personnel_list": [],
-			        "module_equipment": [],
-			        "module_supplies": [],
-			        "module_clinics": [],
-			        "module_working_part_labels": [],
-			        "module_contraindications_labels": [],
-			        "equipment_indications_labels": [],
-			        "module_function_labels": [],
-			        "gender_limit_name": "",
-			        "last_id": '',
-			        "next_id": ''
-				}
+				detail: {},
+				edit_id: this.$route.query.id
 			}
 		},
 		created () {
@@ -147,7 +136,7 @@
 			getDetail () {
 				var self = this;
 				var params = {
-					module_id: this.$route.query.id
+					module_id: this.edit_id
 				}
 				Public.Ajax('module/detail', params, 'GET', function(res){
 					self.detail = res.data;
@@ -156,13 +145,17 @@
 			prevPage () {
 				console.log('prevPage');
 			},
-			editPage () {
-				window.location.href = '#/form' + '?type=modular';
+			editPage (id) {
+				window.location.href = '#/form' + '?type=modular&module_id=' +  id;
 			},
 			deletePage (e) {
-				var id = this.$route.query.id;
+				var id = this.edit_id;
 				Public.deleteModal(e, id, function () {
 					console.log(id);
+					return;
+					Public.Ajax('module/del', {module_id: id}, 'GET', function(res){
+						window.location.href = '#/modular';
+					});
 				});
 			},
 			printPage () {
