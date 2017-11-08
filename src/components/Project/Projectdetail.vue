f2b7c47ef2b139106d2435682377db441c00307d
liufei <1196778674@qq.com>
2017-11-08 16:33:34 +0800

    project detail

--- src/components/Project/Projectdetail.vue
+++ src/components/Project/Projectdetail.vue
@@ -4,91 +4,105 @@
 		    <tbody>
 		        <tr>
 		            <td class="title no-right space">项目编码</td>
-		            <td class="space">A-23344</td>
+		            <td class="space">{{detail.code}}</td>
 		            <td class="title no-right space">项目中心</td>
-		            <td class="space">测试中心</td>
+		            <td class="space">{{detail.center_name}}</td>
 		            <td class="title no-right space">项目名称</td>
-		            <td class="no-right space">项目模块</td>
+		            <td class="no-right space">{{detail.name}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right space">成本</td>
-		            <td class="space">A23324234</td>
+		            <td class="space">{{detail.expected_cost}}</td>
 		            <td class="title no-right space">会员价</td>
-		            <td class="space">122</td>
+		            <td class="space">{{detail.member_price}}</td>
 		            <td class="title no-right space">市场价</td>
-		            <td class="no-right space">100</td>
+		            <td class="no-right space">{{detail.market_price}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right space">年龄限制</td>
-		            <td class="space">30</td>
+		            <td class="space">{{detail.age_limit}}</td>
 		            <td class="title space no-right">性别限制</td>
-		            <td class="space">女</td>
+		            <td class="space">{{detail.gender_limit_name}}</td>
 		            <td class="title no-right space">是否医疗</td>
-		            <td class="no-right space">是</td>
+		            <td class="no-right space">{{detail.whether_medical_name}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right space">项目时间</td>
-		            <td class="space">100</td>
+		            <td class="space">{{detail.time}}</td>
 		            <td class="title no-right space">诊室</td>
-		            <td colspan="3" class="no-right">诊室1,诊室2,诊室3,诊室4,诊室5</td>
+		            <td colspan="3" class="no-right">
+		            	<span v-for="item in detail.module_clinics">{{item.name}}</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right space">执行人等级</td>
-		            <td class="space">专家康复</td>
+		            <td class="space">
+		            	<span v-for="item in detail.job_grades">{{item.name}}</span>
+		            </td>
 		            <td class="title no-right space">执行人</td>
 		            <td colspan="3" class="no-right">专家康复师</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">项目类别</td>
-		            <td colspan="5" class="no-right">项目类别项目类别项目类别</td>
+		            <td colspan="5" class="no-right">{{detail.category_name}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">作用部位</td>
-		            <td colspan="5" class="no-right">腰，腿</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="item in detail.module_working_part_labels">{{item.name}}</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">作用功能</td>
-		            <td colspan="5" class="no-right">检测、报告</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="item in detail.module_function_labels">{{item.name}}</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">项目模板</td>
 		            <td colspan="5" class="no-right">
-		            	<p>进行精准运动诊疗的基础模板1</p>
-		            	<p>进行精准运动诊疗的基础模板2</p>
-		            	<p>进行精准运动诊疗的基础模板3</p>
+		            	<p v-for="item in detail.module_list">{{item.index}}:{{item.name}}</p>
 		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">项目描述</td>
-		            <td colspan="5" class="no-right">进行精准运动诊疗的基础。量化、科学的评估可综合而深度的反映出受评者的体脂率、姿势表现、平衡能力、本体感觉、肌肉能量、关节灵活度等，在测试中发现已经存在或潜在的问题，从而有针对性的设计解决方案。</td>
+		            <td colspan="5" class="no-right">{{detail.description}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">适应症</td>
-		            <td colspan="5" class="no-right">孕妇、骨质疏松、心血管疾病、损伤达手术要求、静脉曲张(严重)、脊髓压迫(严重)、皮肤疾病、皮肤破损、骨折</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="item in detail.module_indications_labels">{{item.name}}</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">禁忌症</td>
-		            <td colspan="5" class="no-right">孕妇、骨质疏松、心血管疾病、损伤达手术要求、静脉曲张(严重)、脊髓压迫(严重)、皮肤疾病、皮肤破损、骨折</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="item in detail.module_contraindications_labels">{{item.name}}</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">设备</td>
-		            <td colspan="5" class="no-right">空气置换身体成分测试系统、FMS功能性运动筛查套件、Y 平衡检测仪、便携式肌力测试与关节活动度计</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="item in detail.module_equipment">{{item.name}}</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">用品</td>
-		            <td colspan="5" class="no-right">BODPOD呼吸管、 一次性拖鞋</td>
+		            <td colspan="5" class="no-right">
+		            	<span v-for="item in detail.module_supplies">{{item.name}}</span>
+		            </td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">注意事项</td>
-		            <td colspan="5" class="no-right">空气置换身体成分测试系统、FMS功能性运动筛查套件、Y 平衡检测仪、便携式肌力测试与关节活动度计</td>
+		            <td colspan="5" class="no-right">{{detail.considerations}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">不良反应</td>
-		            <td colspan="5" class="no-right">空气置换身体成分测试系统、FMS功能性运动筛查套件、Y 平衡检测仪、便携式肌力测试与关节活动度计</td>
+		            <td colspan="5" class="no-right">{{detail.adverse_reaction}}</td>
 		        </tr>
 		        <tr>
 		            <td class="title no-right">备注</td>
-		            <td colspan="5" class="no-right">进行精准运动诊疗的基础。量化、科学的评估可综合而深度的反映出受评者的体脂率、姿势表现、平衡能力、本体感觉、肌肉能量、关节灵活度等，在测试中发现已经存在或潜在的问题，从而有针对性的设计解决方案。</td>
+		            <td colspan="5" class="no-right">{{detail.remark}}</td>
 		        </tr>
 		    </tbody>
 		</table>
@@ -172,6 +186,9 @@
 		text-overflow:ellipsis;
 		word-break:keep-all; 
 		white-space:nowrap;
+	}
+	table tr td span{
+		margin-right: 10px;
 	}
 	table tr td.no-right{
 		border-right: 0;
