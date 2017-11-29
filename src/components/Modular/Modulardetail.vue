6fe9115d38363411465e7a1f72b6e497c15dbd94
liufei <1196778674@qq.com>
2017-11-29 17:00:24 +0800

    show_remark

--- src/components/Modular/Modulardetail.vue
+++ src/components/Modular/Modulardetail.vue
@@ -120,7 +120,11 @@
 			        </tr>
 			        <tr>
 			            <td class="title no-right">备注</td>
-			            <td colspan="5" class="no-right">{{detail.show_remark}}</td>
+			            <td colspan="5" class="no-right">
+			            	<p v-for="item in detail.show_remark">
+			            		{{item.title}} {{item.content}}
+			            	</p>
+			            </td>
 			        </tr>
 			    </tbody>
 			</table>
